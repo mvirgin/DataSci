cf737471405872e2c5362741c9cdad41caa8b4ef
Matthew Virgin <42557195+mvirgin@users.noreply.github.com>
2023-04-29 01:16:52 -0400

    code complete, writeup not

--- Hw4/Hw4.py
+++ Hw4/Hw4.py
@@ -10,6 +10,9 @@ import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn import linear_model
+from sklearn.metrics import accuracy_score
 
 ### Task 1
 
@@ -26,12 +29,38 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratif
 min_vals = np.amin(X_train, axis = 0)
 max_vals = np.amax(X_train, axis = 0)
 
-X_train_scaled = (X_train - min_vals) / (max_vals - min_vals)
+X_train = (X_train - min_vals) / (max_vals - min_vals)
 
 ## use mins and max's of columns from training set to scale test set
-X_test_scaled = (X_test - min_vals) / (max_vals - min_vals)
+X_test = (X_test - min_vals) / (max_vals - min_vals)
 
 ### Task 2
+
+## Linear SVM
+## Train
+svm_clf = LinearSVC(C=1e4, loss='hinge', max_iter=int(1e5))
+svm_clf.fit(X_train, y_train.ravel())
+## Predict / test set accuracy
+svm_y_pred = svm_clf.predict(X_test)
+print('Accuracy on test set w/ SVM:', np.mean(svm_y_pred==y_test))
+
+## Logistic Regression
+## Train
+lrm_clf = linear_model.LogisticRegression(penalty='l2', C=1.0)
+lrm_clf.fit(X_train, y_train.ravel())
+## Predict / test set accuracy
+## column 0 is predicted probabilities of email not being spam
+## column 1 is of email being spam (in test dataset)
+proba_pred = lrm_clf.predict_proba(X_test)[:, 1]
+lrm_y_pred = proba_pred > 0.5   # convert to binaray predictions
+print('Accuracy on test set w/ LRM:', accuracy_score(y_test, lrm_y_pred))
+
+## TODO: examine coefficients for writeup
+# Again, we can access the parameters of a trained classifier using:
+# print(clf.coef_)
+# print(clf.intercept_)
+
+### Task 3
 
 ## Define dataset class
 class dataset(Dataset):
@@ -47,50 +76,59 @@ class dataset(Dataset):
         return self.length
 
 ## create training and test datasets
-trainset = dataset(X_train_scaled, y_train)
-testset = dataset(X_test_scaled, y_test)
+trainset = dataset(X_train, y_train)
+testset = dataset(X_test, y_test)
 
 ## create their data loaders
 trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
 testloader = DataLoader(testset, batch_size=64, shuffle=False)
 
-print(X_train_scaled.shape)
-print(X_train_scaled.shape[1])
+## define neural network 1
 
-## Deinfe Support Vector Machine (SVM) TODO: this is a fully connected NN
-# class SVM(nn.Module):
-#     def __init__(self, n_input_features):
-#         super(SVM, self).__init__()
-#         self.fc1 = nn.Linear(n_input_features, 32)
-#         self.fc2 = nn.Linear(32, 64)
-#         self.fc3 = nn.Linear(64,1)
-#         torch.nn.lin
-
-#     def forward(self, x):
-#         x = torch.relu(self.fc1(x))
-#         x = torch.relu(self.fc2(x))
-#         x = self.fc3(x)
-#         return x
-
-class L_SVM(nn.Module):
+class Net1(nn.Module):
     def __init__(self, n_input_features):
-        super(L_SVM, self).__init__()
-        self.fc = nn.Linear(n_input_features, 1)
-
+        super(Net1, self).__init__()
+        self.fc1 = nn.Linear(n_input_features, 32)
+        self.fc2 = nn.Linear(32, 64)
+        self.fc3 = nn.Linear(64,1)
     def forward(self, x):
-        x = self.fc(x)  # SVM should output raw score, no activation function
-        return x
-
-## Define logistic regression model
-class LRM(nn.Module):
-    def __init__(self, n_input_features):
-        super(LRM, self).__init__()
-        self.linear = nn.Linear(n_input_features, 1)
-
-    def forward(self, x):
-        x = torch.sigmoid(self.linear(x))
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)         # note that sigmoid is not included
         return x
     
+class Net2(nn.Module):
+    def __init__(self, n_input_features):
+        super(Net2, self).__init__()
+        self.fc1 = nn.Linear(n_input_features, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32,1)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)        
+        x = self.sigmoid(x)
+        return x
+    
+class Net3(nn.Module):
+    def __init__(self, n_input_features):
+        super(Net3, self).__init__()
+        self.fc1 = nn.Linear(n_input_features, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 32)
+        self.fc5 = nn.Linear(32, 1)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = torch.relu(self.fc4(x))
+        x = self.fc5(x)
+        x = self.sigmoid(x)
+        return x
+
 ## define function for training a model TODO: assuming they can all be trained the same way?
 ## takes # of epochs, a trainloader, a loss function, an optimizer, and a model
 ## trains model using the loss function and optimizer given on the data
@@ -120,42 +158,35 @@ def testModel(testloader, model):
         y_test_batch = y_test_batch.detach().numpy()
         acc_test += (pred_test_batch == y_test_batch).sum()
 
-    acc_test /= X_test_scaled.shape[0]
+    acc_test /= X_test.shape[0]
     return acc_test
     
-
 ## Learning rate and epochs for all models
 learning_rate = 0.01
 epochs = 500
 
-## Train SVM
-modelSVM = L_SVM(n_input_features=X_train_scaled.shape[1])
-optimizerSVM = torch.optim.SGD(modelSVM.parameters(),  # best params w grad. desc.
-                            lr=learning_rate, 
-                            weight_decay=.001) # for L^2 regularization
-loss_fnSVM = nn.HingeEmbeddingLoss()   # SVM's minimize sum of hinge losses on train 
-## TODO: slides example uses BCEWithLogitsLoss for loss_fn - is that why the test/train there looks like that? Should mine be different?
-## see https://towardsdatascience.com/logistic-regression-with-pytorch-3c8bbea594be for LRM train/test - very different and my class is modeled off of that
+## Define and train each model
+model1 = Net1(n_input_features=X_train.shape[1])
+optimizer1 = torch.optim.SGD(model1.parameters(), lr=learning_rate, weight_decay=0.001)
 
-print("training SVM")
-trainModel(epochs, trainloader, loss_fnSVM, optimizerSVM, modelSVM)
+model2 = Net2(n_input_features=X_train.shape[1])
+optimizer2 = torch.optim.SGD(model2.parameters(), lr=learning_rate, weight_decay=0.001)
 
-## Test SVM
-print("Accuracy on the test set w/ SVM is: {}".format(testModel(testloader, modelSVM)))
+model3 = Net3(n_input_features=X_train.shape[1])
+optimizer3 = torch.optim.SGD(model3.parameters(), lr=learning_rate, weight_decay=0.001)
 
-## Train LRM
-modelLRM = LRM(n_input_features=X_train_scaled.shape[1])
-## According to lecture, LRM is also best w grad. desc and L^2
-optimizerLRM = torch.optim.SGD(modelLRM.parameters(),  # best params w grad. desc.
-                            lr=learning_rate, 
-                            weight_decay=.001) # for L^2 regularization
-## However, different loss function
-loss_fnLRM = nn.BCEWithLogitsLoss()
+loss_fn = torch.nn.BCEWithLogitsLoss() # sigmoid is included here
 
-print("training LRM")
-trainModel(epochs, trainloader, loss_fnLRM, optimizerLRM, modelLRM)
+print("\nTraining model 1 \n")
+trainModel(epochs, trainloader, loss_fn, optimizer1, model1)
+print("\nTraining model 2 \n")
+trainModel(epochs, trainloader, loss_fn, optimizer2, model2)
+print("\nTraining model 3 \n")
+trainModel(epochs, trainloader, loss_fn, optimizer3, model3)
 
-## Test LRM
-print("Accuracy on the test set w/ LRM is: {}".format(testModel(testloader, modelLRM)))
+## Test each model
+print("\nmodel 1 acc. on test set: {}".format(testModel(testloader, model1)))
+print("model 2 acc. on test set: {}".format(testModel(testloader, model2)))
+print("model 3 acc. on test set: {}".format(testModel(testloader, model3)))
 
 ## TODO: (see lec) NN's dont perform any better than traditional models on linearly seperable data - for writeup
